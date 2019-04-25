7087c52ba8ca9d12c1d0d6005edec9ce4a63616e
Sam <sfurman@vt.edu>
2019-04-25 14:42:05 -0400

    Done with Basics and added some audio samples

--- TextBlobs.py
+++ TextBlobs.py
@@ -11,15 +11,29 @@ class TextBlob():
     def setText(text):
         self.text = text
 
+class Description(TextBlob):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
 
-class Review(TextBlob):
+    def __eq__(self,other):
+        return super().__eq__(self, other)
+
+class Comment(TextBlob):
+    def __init__(self, profile, *args, **kwargs):
+        self.profile = profile
+        super().__init__(self, *args, **kwargs)
+
+    def getProfile():
+        return self.profile
+
+class Review():
     def __init__(self, rating, *args, **kwargs):
         self.rating = rating
         self.comment = kwargs.get('comment', '')
 
     def __eq__(self,other):
         return (self.rating == other.rating 
-        and )
+        and self.comment == other.comment)
 
     def getRating():
         return self.rating
@@ -27,12 +41,9 @@ class Review(TextBlob):
     def setRating(rating):
         self.rating = rating
 
-class Comment(TextBlob):
-    def __init__(self, profile, *args, **kwargs):
-        self.profile = profile
-        super().__init__(self, *args, **kwargs)
+    def getComment():
+        return self.comment
 
-    def 
+    def setComment(comment):
+        self.comment = comment
 
-class Description(TextBlob):
-    pass
